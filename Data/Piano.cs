0a7c8f0a1e7be4490414dedd4475f22f34a59638
Alexander Arvidsson <to.alexander.a@gmail.com>
2021-11-24 15:19:09 +0100

    Starting piano
    Piano functions added

---
+++ Data/Piano.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using SharpDX.DirectSound;
+
+namespace MusicAuthenticationSystem.Data
+{
+    public class Piano
+    {
+        public List<string> PlayedNotes { get; set; }
+
+        public Piano()
+        {
+            PlayedNotes = new List<string>();
+        }
+    }
+}
--- Startup.cs
+++ Startup.cs
@@ -29,6 +29,7 @@ namespace MusicAuthenticationSystem
             services.AddRazorPages();
             services.AddServerSideBlazor();
             services.AddSingleton<WeatherForecastService>();
+            services.AddSingleton<PianoService>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
